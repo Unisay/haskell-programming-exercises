02cdc4ca6a6cf02ba9d859ebc4f2770623e0f369
Yuriy Lazaryev <Yuriy.Lazaryev@zalando.de>
2017-01-18 22:50:17 +0000

    break words excercise

--- src/Lists.hs
+++ src/Lists.hs
@@ -23,3 +23,22 @@ eftEnum from to
   | from < to = from : (eftEnum (succ from) to)
   | from == to = [from]
   | otherwise = []
+
+{-
+Using takeWhile and dropWhile, write a function that takes a string
+and returns a list of strings, using spaces to separate the elements
+of the string into words, as in the following sample:
+*Main> myWords "all i wanna do is have some fun"
+["all","i","wanna","do","is","have","some","fun"]
+-}
+
+breakWords :: Char -> String -> [String]
+breakWords _ "" = []
+breakWords c s  = head : breakWords c tail
+  where
+    isSeparator = (== c)
+    head = takeWhile (not . isSeparator) s
+    tail = drop 1 (dropWhile (not . isSeparator) s)
+
+myWords :: String -> [String]
+myWords = breakWords ' '
