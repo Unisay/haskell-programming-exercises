7b68669b162dc15ebc7626f49133899d84d82c25
Yuriy Lazarev <unisay@gmail.com>
2017-01-30 22:46:37 +0100

    ADTs

---
+++ src/ADTs.hs
@@ -0,0 +1,37 @@
+module ADTs where
+
+{-# ANN module "HLint: ignore Use camelCase" #-}
+
+data Price = Price Integer deriving (Eq, Show)
+data Manufacturer = Mini | Mazda | Tata deriving (Eq, Show)
+data Airline = PapuAir | CatapultsR'Us | TakeYourChancesUnited deriving (Eq, Show)
+data PlaneSize = Small | Normal | Huge deriving (Eq, Show)
+data Vehicle = Car Manufacturer Price | Plane Airline PlaneSize deriving (Eq, Show)
+
+
+myCar :: Vehicle
+myCar = Car Mini (Price 14000)
+
+urCar :: Vehicle
+urCar = Car Mazda (Price 20000)
+
+clownCar :: Vehicle
+clownCar = Car Tata (Price 7000)
+
+doge :: Vehicle
+doge = Plane PapuAir Huge
+
+isCar :: Vehicle -> Bool
+isCar (Car _ _) = True
+isCar _ = False
+
+isPlane :: Vehicle -> Bool
+isPlane (Plane _ _) = True
+isPlane _ = False
+
+areCars :: [Vehicle] -> [Bool]
+areCars = map isCar
+
+getManu :: Vehicle -> Maybe Manufacturer
+getManu (Car m _) = Just m
+getManu _ = Nothing
