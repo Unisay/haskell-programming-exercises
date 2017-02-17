d946101e137f3cc60041578e5bc4cdd486f1a9bf
Yuriy Lazarev <unisay@gmail.com>
2017-02-17 18:34:17 +0100

    BinTree

---
+++ src/adt/BinTree.hs
@@ -0,0 +1,62 @@
+module BinTree where
+
+data BinaryTree a = Leaf | Node (BinaryTree a) a (BinaryTree a) deriving (Eq, Ord, Show)
+
+foldTree :: (a -> b -> b) -> b -> BinaryTree a -> b
+foldTree _ z Leaf = z
+foldTree f z (Node l a r) = foldTree f (foldTree f (f a z) l) r
+
+mapTree :: (a -> b) -> BinaryTree a -> BinaryTree b
+mapTree _ Leaf = Leaf
+mapTree f (Node left a right) = Node (mapTree f left) (f a) (mapTree f right)
+
+testTree' :: BinaryTree Integer
+testTree' = Node (Node Leaf 3 Leaf) 1 (Node Leaf 4 Leaf)
+
+testMap :: IO ()
+testMap =
+  if mapTree (+1) testTree' == Node (Node Leaf 4 Leaf) 2 (Node Leaf 5 Leaf)
+  then print "yup okay!"
+  else error "test failed!"
+
+
+preorder :: BinaryTree a -> [a]
+preorder Leaf = []
+preorder (Node left a right) = [a] ++ preorder left ++ preorder right
+
+inorder :: BinaryTree a -> [a]
+inorder Leaf = []
+inorder (Node left a right) = inorder left ++ [a] ++ inorder right
+
+postorder :: BinaryTree a -> [a]
+postorder Leaf = []
+postorder (Node left a right) = postorder left ++ postorder right ++ [a]
+
+
+testTree :: BinaryTree Integer
+testTree = Node (Node Leaf 1 Leaf) 2 (Node Leaf 3 Leaf)
+
+testPreorder :: IO ()
+testPreorder =
+  if preorder testTree == [2, 1, 3]
+  then putStrLn "Preorder fine!"
+  else putStrLn "Bad news bears."
+
+testInorder :: IO ()
+testInorder =
+  if inorder testTree == [1, 2, 3]
+  then putStrLn "Inorder fine!"
+  else putStrLn "Bad news bears."
+
+testPostorder :: IO ()
+testPostorder =
+  if postorder testTree == [1, 3, 2]
+  then putStrLn "Postorder fine!"
+  else putStrLn "postorder failed check"
+
+main :: IO ()
+main = do
+  testMap
+  testPreorder
+  testInorder
+  testPostorder
