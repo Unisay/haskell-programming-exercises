f39eb4477c2081c0c4e2a6f7e8f766aec673e4bd
Yuriy Lazarev <unisay@gmail.com>
2017-02-26 14:56:09 +0100

    Reading input from console

--- src/adt/Vigenere.hs
+++ src/adt/Vigenere.hs
@@ -1,7 +1,11 @@
 module Vigenere where
 
-import Data.Char
-import Data.List()
+import           Control.Monad (forever, when)
+import           Data.Char
+import           Data.List
+
+newtype Err = MkError String
+data Mode = Encode | Decode
 
 enumerate :: String -> [Int]
 enumerate = cycle . map ord
@@ -24,10 +28,41 @@ shiftL :: Char -> Int -> Char
 shiftL c i = shiftR c (1 - i)
 
 shiftWith :: (Char -> Int -> Char) -> String -> String -> String
-shiftWith f pass s = zipWith f s (enumerate password)
+shiftWith f pass s = zipWith f s (enumerate pass)
 
 encode :: String -> String -> String
 encode = shiftWith shiftR
 
 decode :: String -> String -> String
 decode = shiftWith shiftL
+
+welcome :: IO ()
+welcome = putStrLn "Welcome to the Vigenere cipher Encoder/Decoder!"
+
+readLine :: String -> IO String
+readLine msg = do
+  putStrLn msg
+  getLine
+
+getMode :: IO (Either Err Mode)
+getMode = do
+  input <- readLine "In order to encode message press E, for decoding press D"
+  return $ case input of
+    ['D'] -> Right Decode
+    ['E'] -> Right Encode
+    _     -> Left $ MkError "Neither E nor D was entered!"
+
+main :: IO ()
+main = do
+  welcome
+  forever $ do
+  errorOrMode <- getMode
+  case errorOrMode of
+    (Right Encode) -> handle encode "encode"
+    (Right Decode) -> handle decode "decode"
+    (Left (MkError errorMsg)) -> putStrLn errorMsg
+    where
+      handle f action = do
+        word <- readLine $ "Please enter a word to " ++ action ++ " with the Vigenere cipher:"
+        pass <- readLine "Please enter password:"
+        putStrLn $ "Result: " ++ f pass word
