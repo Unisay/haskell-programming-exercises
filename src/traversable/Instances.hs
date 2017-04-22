2ac7dc0df3bb56ffe3d4cd9e650438c8d2fe852b
Yuriy Lazarev <unisay@gmail.com>
2017-04-22 17:07:44 +0200

    Traversable instances for Constant, List, Optional

--- src/traversable/Instances.hs
+++ src/traversable/Instances.hs
@@ -1,7 +1,7 @@
 module Instances where
 
 newtype Identity a = Identity a
-  deriving (Eq, Ord, Show)
+  deriving (Eq, Show)
 
 instance Functor Identity where
   fmap f (Identity x) = Identity $ f x
@@ -12,3 +12,47 @@ instance Foldable Identity where
 instance Traversable Identity where
   traverse f (Identity x) = Identity <$> f x
 
+
+newtype Constant a b = Constant { getConstant :: a }
+  deriving (Eq, Show)
+
+instance Functor (Constant a) where
+  fmap _ (Constant x) = Constant x
+
+instance Foldable (Constant a) where
+  foldMap _ (Constant _) = mempty
+
+instance Traversable (Constant a) where
+  traverse _ (Constant x) = pure $ Constant x
+
+
+data List a = Nil | Cons a (List a)
+  deriving (Eq, Show)
+
+instance Functor List where
+  fmap _ Nil = Nil
+  fmap f (Cons h t) = Cons (f h) $ fmap f t
+
+instance Foldable List where
+  foldr _ z Nil = z
+  foldr f z (Cons h t) = foldr f (f h z) t
+
+instance Traversable List where
+  traverse _ Nil = pure Nil
+  traverse f (Cons h t) = Cons <$> f h <*> traverse f t
+
+
+data Optional a = Nada | Yep a
+  deriving (Eq, Show)
+
+instance Functor Optional where
+  fmap _ Nada = Nada
+  fmap f (Yep a) = Yep (f a)
+
+instance Foldable Optional where
+  foldMap _ Nada = mempty
+  foldMap f (Yep x) = f x
+
+instance Traversable Optional where
+  traverse _ Nada = pure Nada
+  traverse f (Yep x) = Yep <$> f x
