b9aaeb90ae759b6cb5c648116e26dbe466579f40
Yuriy Lazarev <unisay@gmail.com>
2017-04-24 21:49:06 +0200

    Reader applicative

--- src/reader/ThisIsReader.hs
+++ src/reader/ThisIsReader.hs
@@ -27,3 +27,11 @@ tupledM = do
 
 tupledB :: [Char] -> ([Char], [Char])
 tupledB = rev >>= (,) . cap
+
+newtype Reader r a = Reader { runReader :: r -> a }
+
+instance Functor (Reader r) where
+  fmap f (Reader ra) = Reader (f . ra)
+
+ask :: Reader a a
+ask = Reader id
