cbc111878e93bd05cf150c431255ae500d69b918
Yuriy Lazaryev <Yuriy.Lazaryev@zalando.de>
2017-05-18 11:45:35 +0200

    Polymorphic parsers

--- src/parser/Fractions.hs
+++ src/parser/Fractions.hs
@@ -1,10 +1,26 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Text.Fractions where
 
 import Text.Trifecta
 import Control.Applicative
 import Data.Ratio ((%))
+import Data.String (IsString)
+import Data.Attoparsec.Text (parseOnly)
 
-parseFraction :: Parser Rational
+badFraction :: IsString s => s
+badFraction = "1/0"
+
+alsoBad :: IsString s => s
+alsoBad = "10"
+
+shouldWork :: IsString s => s
+shouldWork = "1/2"
+
+shouldAlsoWork :: IsString s => s
+shouldAlsoWork = "2/1"
+
+parseFraction :: (Monad m, TokenParsing m) => m Rational
 parseFraction = do
   numerator <- decimal
   _ <- char '/'
@@ -20,6 +36,13 @@ parseDecOrFrac = dec <|> frac where
 
 main :: IO ()
 main = do
+  -- parseOnly is Attoparsec
+  print $ parseOnly parseFraction badFraction
+  print $ parseOnly parseFraction shouldWork
+  print $ parseOnly parseFraction shouldAlsoWork
+  print $ parseOnly parseFraction alsoBad
+
+  -- parseString is Trifecta
   print $ parseString parseFraction mempty "1/2"
   print $ parseString parseFraction mempty "2/1"
   print $ parseString parseDecOrFrac mempty "10"
