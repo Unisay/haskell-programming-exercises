9ac854b555a598d3a7bddf9fb86980958aa6bb7f
Yuriy Lazarev <unisay@gmail.com>
2017-05-25 15:08:33 +0200

    swapEitherT, eitherT

--- src/transformers/EitherT.hs
+++ src/transformers/EitherT.hs
@@ -2,6 +2,8 @@
 
 module EitherT where
 
+import Data.Either (either)
+
 newtype EitherT e m a = EitherT { runEitherT :: m (Either e a) }
 
 instance Functor m => Functor (EitherT e m) where
@@ -14,7 +16,17 @@ instance Applicative m => Applicative (EitherT e m) where
 instance Monad m => Monad (EitherT e m) where
   return = pure
   (>>=) :: forall a b . EitherT e m a -> (a -> EitherT e m b) -> EitherT e m b
-  (EitherT m) >>= f = EitherT $ m >>= g where
+  (EitherT m) >>= f = EitherT $ m >>= g
+    where
     g :: Either e a -> m (Either e b)
     g (Left e) = return $ Left e
     g (Right a) = runEitherT (f a)
+
+swapEitherT :: Functor m => EitherT e m a -> EitherT a m e
+swapEitherT et = EitherT $ swapEither <$> runEitherT et
+  where
+  swapEither (Left l) = Right l
+  swapEither (Right r) = Left r
+
+eitherT :: Monad m => (a -> m c) -> (b -> m c) -> EitherT a m b -> m c
+eitherT f g et = runEitherT et >>= either f g
