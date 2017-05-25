d1bd62c8ec95ce6f4f1415094b2bea63cc91c890
Yuriy Lazarev <unisay@gmail.com>
2017-05-25 17:29:20 +0200

    StateT

---
+++ src/transformers/StateT.hs
@@ -0,0 +1,19 @@
+module StateT where
+
+newtype StateT s m a = StateT { runStateT :: s -> m (a, s) }
+
+instance Functor m => Functor (StateT s m) where
+  fmap f (StateT g) = StateT $ \s -> c <$> g s where c (a, s) = (f a, s)
+
+instance Monad m => Applicative (StateT s m) where
+  pure x = StateT $ \s -> pure (x, s)
+  (<*>) (StateT smf) (StateT smr) = StateT $ \s0 -> do
+    (f, s1) <- smf s0
+    (r, s2) <- smr s1
+    return (f r, s2)
+
+instance (Monad m) => Monad (StateT s m) where
+  return = pure
+  (>>=) (StateT sma) f = StateT $ \s -> do
+    (a, s') <- sma s
+    runStateT (f a) s'
